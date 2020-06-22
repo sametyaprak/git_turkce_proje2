55488ec213065bfa2eeb4e0384d932bfead5c013
sametyaprak <sametyaprak@gmail.com>
2020-06-22 18:18:49 +0200

    ikinci commit

--- src/git_turkce_proje2/Git_turkce_proje2.java
+++ src/git_turkce_proje2/Git_turkce_proje2.java
@@ -5,6 +5,7 @@ public class Git_turkce_proje2 {
 	public static void main(String[] args) {
 		
 		System.out.println("ilk commit from local repository");
+		System.out.println("ikinci commit from local repository");
 		
 	}
 
