f0280a4e298702acff3ef403d41ee234259ce5c3
sametyaprak <sametyaprak@gmail.com>
2020-06-22 18:11:38 +0200

    ilk commit local repositoryden

---
+++ src/git_turkce_proje2/Git_turkce_proje2.java
@@ -0,0 +1,11 @@
+package git_turkce_proje2;
+
+public class Git_turkce_proje2 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("ilk commit from local repository");
+		
+	}
+
+}
